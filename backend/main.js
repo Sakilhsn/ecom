5734950c0ef6af5fd0f2815d4393736cc083d74b
Sakilhsn <sakilhossain.com@gmail.com>
2024-01-03 14:19:50 +0530

    Project setup done

---
+++ backend/main.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+// middleware
+const middleware = (req,res,next)=>{
+    console.log('hello my middleware');
+    next()
+}
+
+app.get('/',middleware, (req,res)=> {
+    res.send ('Hello world form the server');
+});
+app.get('/contact', (req,res)=> {
+    res.send ('Hello contact the world form the server');
+});
+app.get('/about', (req,res)=> {
+    res.send ('Hello about the world form the server');
+});
+app.get('/login', (req,res)=> {
+    res.send ('Hello world form the server in login');
+});
+
+
+app.listen(4000,()=> {
+    console.log(' server is running at port number 4000');
+});
