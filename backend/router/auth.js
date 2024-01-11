71048920e31511700754e317aeeb054a41218847
Sakilhsn <sakilhossain.com@gmail.com>
2024-01-11 13:57:06 +0530

    added sign

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -49,9 +49,42 @@ router.post('/register', async (req,res) =>{
        }
        
 
-    }catch(err){
+        }catch(err){
         console.log(err)
     }
+});
+
+// login route promise
+
+//router.post('./signin', (req,res) => {
+//console.log(req.body);
+//res.json({message:" awesome"})
+
+//})
+
+//log in validation check using async
+
+router.post('/signin', async (req,res) => {
+ try{
+    const {email, password} = req.body;
+     
+    if (!email || !password) {
+      return  res.status(400).json({error: "please filled the field"})
+      }
+
+      const userlogin = await User.findOne({email:email});
+      console.log(userlogin);
+      if (!userlogin){
+        res.status(400).json({error: "user error"});
+      }else{
+        res.json({message:"user signin successfully"});
+      }
+      
+
+
+  }catch (err) {
+     console.log(err);
+  }
 });
 
 // res.json({message: req.body});
