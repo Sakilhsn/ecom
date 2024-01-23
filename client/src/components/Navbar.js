90b226bb727c1a473cd72a223c9eb4711df32a8f
Sakilhsn <sakilhossain.com@gmail.com>
2024-01-23 15:17:24 +0530

    added css and images and sign in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css' ;
 import { NavLink } from 'react-router-dom';
+import logo from "../Images/logo2.png" ;
 
 const Navbar = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{justifyContent:'space-between'}}>
-      
-    <a className="navbar-brand" href="/">Navbar</a>
+    <NavLink className="navbar-brand" to="#">
+      <img src={logo} alt='logo'/>
+    </NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{flexGrow:0}}>
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
-          <a className="nav-link " href="/">Home  <span className ="sr-only">(current)</span></a>
-        </li>
+          <NavLink className="nav-link " to="/">Home  <span className ="sr-only">(current)</span></NavLink>
+        </li>                         
         <li className="nav-item">
-          <NavLink className="nav-link" href="/About">About</NavLink>
+          <NavLink className="nav-link" to="/About">About</NavLink>
         </li>
 
         <li className="nav-item">
-          <a className="nav-link" href="/Contact">Contact</a>
+          <NavLink className="nav-link" to="/Contact">Contact</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/Login">login</a>
+          <NavLink className="nav-link" to="/Login">login</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/Signup">Registration</a>
+          <NavLink className="nav-link" to="/Signup">Registration</NavLink>
         </li>
     
       </ul>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,12 +1,75 @@
 import React from 'react'
+import signpic from "../Images/logo1.png";
 
 const Signup = () => {
   return (
-    <div> 
-        <p>WELCOME</p>
-        <h1> Hy we are mern signup  developer</h1>
+    <>
+    <section className='signup'>
+    <img src={signpic} alt='signpic'/> 
+    <div className='container mt-5'>
+        <div className='signup-content'>
+          <div className='signup-form'>
+            <h2 className='form-title'> Signup</h2>
+            <form className='register-form' id='register-form'> 
+               <div className='form-group'>
+                 <label htmlFor='name'>
+               <i class="zmdi zmdi-account material-icons-name"></i>
+              </label>
+              <input type='text' name='name' id='naame' autoComplete='off' placeholder='Write your name'></input>
+
+            </div>
+            <div className='form-group'>
+                 <label htmlFor='email'>
+               <i class="zmdi zmdi-email material-icons-name"></i>
+              </label>
+              <input type='email' name='email' id='email' autoComplete='off' placeholder='Write your email'></input>
+        
+            </div>
+            <div className='form-group'>
+                 <label htmlFor='phone'>
+               <i class="zmdi zmdi-phone material-icons-name"></i>
+              </label>
+              <input type='number' name='phone' id='phone' autoComplete='off' placeholder='Write your phone'></input>
+
+            </div>
+            <div className='form-group'>
+                 <label htmlFor='work'>
+               <i class="zmdi zmdi-slideshow material-icons-name"></i>
+              </label>
+              <input type='text' name='work' id='work' autoComplete='off' placeholder='Write your work'></input>
+
+            </div>
+
+            <div className='form-group'>
+                 <label htmlFor='password'>
+               <i class="zmdi zmdi-lock material-icons-name"></i>
+              </label>
+              <input type='text' name='password' id='password' autoComplete='off' placeholder='Write your password'></input>
+
+            </div>
+
+            <div className='form-group'>
+                 <label htmlFor='cpassword'>
+               <i class="zmdi zmdi-lock material-icons-name"></i>
+              </label>
+              <input type='text' name='cpassword' id='cpassword' autoComplete='off' placeholder='Write your cpassword'></input>
+
+            </div>
+            </form>
+      
+
+          </div>
+
+
+        </div>
+
     </div>
-  )
+    
+    
+    </section>
+    
+    </>
+  );
 }
 
 export default Signup
